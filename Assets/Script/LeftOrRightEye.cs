e81c740694bcc5c7fb5e39a042662ea5c28d635f
Gleb <glchernenko1@gmail.com>
2022-03-27 00:01:05 +0300

    first step

---
+++ Assets/Script/LeftOrRightEye.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftOrRightEye : MonoBehaviour
+{
+    [SerializeField] MeshRenderer renderer;
+        [SerializeField] bool right = true;
+       
+        // Start is called before the first frame update
+        void Start()
+        {
+            renderer = GetComponent<MeshRenderer>();
+           
+            renderer.material.SetFloat("IndexEye", right ? 1 : 0);
+            renderer.material.color = right ?  Color.green : Color.red;
+        }
+}
